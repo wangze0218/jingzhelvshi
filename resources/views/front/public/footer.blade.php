6be3d0c4364c8b71cfdf59a2ef679aabeceb9981
wangze <wangze0218@qq.com>
2017-12-21 11:40:16 +0800

    底部样式

--- resources/views/front/public/footer.blade.php
+++ resources/views/front/public/footer.blade.php
@@ -1,38 +1,110 @@
 <style>
-
-    #copyright {
-        background-color: #363636;
-    }
-    .title h4 {
-        font-family: "Microsoft YaHei", "宋体", "Helvetica Neue",Helvetica,Arial,sans-serif;
-        font-size: 22px;
-        font-weight: bold;
-        letter-spacing: 4px;
-        text-indent: 6px;
-        color: #2f2e2e;
-    }
-
-    #copyright p {
+    .copyright {
+        background: #111;
+        font-size: 13px;
         text-align: center;
-        margin: 10px;
-        line-height: 40px;
-        color: #fafafa;
+        color: #555555;
+        padding-top: 28px;
+        padding-bottom: 28px;
+        border-top: 1px solid #303030;
     }
-    #copyright p a {
-        color: #fafafa;
+    .copyright span {
+        margin: 0 .5em;
     }
 </style>
+<style>
+    .main-footer {
+        margin-top: 50px;
+        background: #202020;
+        padding: 35px 0 0;
+        color: #959595;
+    }
+    article, aside, details, figcaption, figure, footer, header, hgroup, main, menu, nav, section, summary {
+        display: block;
+    }
+    .main-footer .widget {
+        padding: 0px 30px;
+    }
+    .widget {
+        margin-bottom: 35px;
+    }
+    .main-footer .widget .title {
+        color: #ffffff;
+        border-bottom: 1px solid #303030;
+    }
+    .widget .title {
+        margin-top: 0;
+        padding-bottom: 7px;
+        border-bottom: 1px solid #ebebeb;
+        margin-bottom: 21px;
+        position: relative;
+    }
+    h4 {
+        font-size: 1.5em;
+    }
+    .main-footer {
+        background: #202020;
+        padding: 35px 0 0;
+        color: #959595;
+    }
+    a {
+        border: none;
+    }
+    .main-footer .widget .tag-cloud a {
+        border: 1px solid #303030;
+    }
+    .widget .tag-cloud a {
+        border: 1px solid #ebebeb;
+        padding: 2px 7px;
+        color: #959595;
+        line-height: 1.5em;
+        display: inline-block;
+        margin: 0 7px 7px 0;
+        -webkit-transition: all 0.2s ease;
+        -o-transition: all 0.2s ease;
+        transition: all 0.2s ease;
+    }
+</style>
+<footer class="main-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="widget">
+                    <h4 class="title">友链</h4>
+                    <div class="content tag-cloud friend-links">
+                        <a href=""  target="_blank">XXX律师</a>
+                        <a href=""  target="_blank">XXX律师</a>
+                    </div>
+                </div>
+            </div>
 
-<div id="copyright">
+            {{--<div class="col-sm-4">--}}
+                {{--<div class="widget">--}}
+                    {{--<h4 class="title">标签云</h4>--}}
+                    {{--<div class="content tag-cloud">--}}
+                        {{--<a href="/tag-cloud/">...</a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+            {{--<div class="col-sm-4">--}}
+                {{--<div class="widget">--}}
+                    {{--<h4 class="title">合作伙伴</h4>--}}
+                    {{--<div class="content tag-cloud friend-links">--}}
+                        {{--<a href="http://www.qiniu.com/" title="七牛云存储" onclick="_hmt.push(['_trackEvent', 'link', 'click', 'qiniu'])" target="_blank">七牛云存储</a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        </div>
+    </div>
+</footer>
+<div class="copyright">
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <p>Copyright © 2006-2017 clantrip.com All Rights Reversed. 律师 <a target="_blank" href="http://www.miitbeian.gov.cn/">京ICP备XXXXXXX号号</a></p>
-                <div style="width:300px;margin:0 auto; padding:20px 0; text-align: center;">
-                    <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=XXXXXXX" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-                        <img src="http://www.clantrip.com/static/images/ghs.png" style="float:left;">
-                        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#fafafa;">京公网安备 XXXXXXX号</p></a>
-                </div>
+                <span>Copyright © <a href="http://www.ghostchina.com/">德胜房产律师</a></span> |
+                <span><a href="http://www.miibeian.gov.cn/" target="_blank">京ICP备11008151号</a></span> |
+                <span>京公网安备11010802014853</span>
             </div>
         </div>
     </div>
